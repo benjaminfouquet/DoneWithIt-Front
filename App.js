2f056bbb327a179e4530fea51f3026f601429724
benjaminfouquet <bfouquet.123@gmail.com>
2023-11-30 12:05:09 +1100

    Splash screen (some problems)

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { useNetInfo } from "@react-native-community/netinfo";
 import { jwtDecode } from "jwt-decode";
 import "core-js/stable/atob"; // <- polyfill here
-import { AppLoading } from "expo";
+import * as SplashScreen from "expo-splash-screen";
 
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import AppNavigator from "./app/navigation/AppNavigator";
@@ -12,6 +12,9 @@ import Screen from "./app/components/Screen";
 import OfflineNotice from "./app/components/OfflineNotice";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const netInfo = useNetInfo();
@@ -24,14 +27,35 @@ export default function App() {
     setUser(jwtDecode(token));
   };
 
+  const onRestore = async () => {
+    if (isReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  };
+
   useEffect(() => {
-    restoreToken();
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await restoreToken();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
   }, []);
 
-  // if (!isReady)
-  //   return (
-  //     <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
-  //   );
+  if (!isReady) return null;
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
