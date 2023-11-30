152e856982ecf768f55d5a415887112e773eaa4a
benjaminfouquet <bfouquet.123@gmail.com>
2023-11-30 12:25:25 +1100

    keep user logged in + decode token in app

--- App.js
+++ App.js
@@ -27,32 +27,11 @@ export default function App() {
     setUser(jwtDecode(token));
   };
 
-  const onRestore = async () => {
-    if (isReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync();
-    }
-  };
-
   useEffect(() => {
-    async function prepare() {
-      try {
-        // Pre-load fonts, make any API calls you need to do here
-        await restoreToken();
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setIsReady(true);
-        await SplashScreen.hideAsync();
-      }
-    }
-
-    prepare();
+    restoreToken().then(() => {
+      setIsReady(true);
+      SplashScreen.hideAsync();
+    });
   }, []);
 
   if (!isReady) return null;
